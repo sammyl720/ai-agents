956f99cdb36ef6898e01a3d133163dfd3f430bfb
sam <sam.leider@hotmail.com>
2024-12-05 19:53:38 -0500

    remove alias in test

--- src/parsers/agent.ts
+++ src/parsers/agent.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { DEFAULT_OPENAI_MODEL } from '@consts';
+import { DEFAULT_OPENAI_MODEL } from '../consts.js';
 
 export const AgentInitConfiguration = z.object({
 	name: z.string().regex(new RegExp('^[a-zA-Z0-9_- ]{5,25}$')),
