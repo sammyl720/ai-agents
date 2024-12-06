8059316151f05d73d0d471d21f8043d9c5adce00
sam <sam.leider@hotmail.com>
2024-12-05 19:58:54 -0500

    fix regex error

--- src/parsers/agent.ts
+++ src/parsers/agent.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
 import { DEFAULT_OPENAI_MODEL } from '../consts.js';
 
+const nameRegex = new RegExp('^[a-zA-Z0-9_ -]{5,25}$');
 export const AgentInitConfiguration = z.object({
-	name: z.string().regex(new RegExp('^[a-zA-Z0-9_- ]{5,25}$')),
+	name: z.string().regex(nameRegex),
 	description: z.string().min(20).max(500),
 	model: z.string().default(DEFAULT_OPENAI_MODEL),
 });
